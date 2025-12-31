66cf37c251fbaa0c712801ca4ee2a90ee81dc73d
rubenibarra005 <198497967+rubenibarra005@users.noreply.github.com>
2025-12-31 10:02:10 -0600

    Act

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
 import mysql.connector
 import os
 from dotenv import load_dotenv
+import pandas as pd
+import streamlit as st
 
 load_dotenv()
 
-with mysql.connector.connect(
+def crearSitio():
+    st.title("Dataset Delitos México")
+
+
+def get_connection():
+    return mysql.connector.connect(
     host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASS"),
     database= os.getenv("DB_NAME"),
-) as conn:
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT COUNT(*) FROM delitos")
-        print(cursor.fetchone())
+    )
 
-query = "SELECT * FROM delitos WHERE entidad like  'Ciudad %';"
+#Queries
+delitoCiudad = """SELECT * FROM delitos 
+        WHERE entidad like %s
+        AND tipo_delito = %s;"""
+
+registrosAnio = """SELECT anio, 
+            COUNT(*) as numero_de_delitos FROM delitos 
+            GROUP BY anio;"""
+
+#with get_connection() as conn:
+ #   df = pd.read_sql(delitoCiudad, conn, params=("Ciudad %","Despojo",))
+
+#df.to_csv("despojosCdmx.csv")
+
+with get_connection() as conn:
+    registrosAnioDF = pd.read_sql(registrosAnio, conn)
+
+#print(registrosAnioDF)
+
+crearSitio()
